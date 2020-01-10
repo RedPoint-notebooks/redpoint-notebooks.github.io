99bf68cd9db50f3725b65e9d3e42c324c684326b
ridergit <benjaminriderharvey@gmail.com>
2020-01-10 13:44:13 -0500

    Adds slideshow for mutating cloned notebook

--- javascripts/application.js
+++ javascripts/application.js
@@ -9,45 +9,51 @@ $(function() {
   $nextButton.on("click", function(e) {
     e.preventDefault();
 
-    if (image.src.match("slide_1")) {
-      image.src = "images/diagrams/slides/hidden_state/slide_2.png";
-      $image
-        .next("p")
-        .text("Typo is fixed, and cell is run again resulting in an error.");
-    } else if (image.src.match("slide_2")) {
-      $nextButton.prop("disabled", true);
-    }
+    image.src = "images/diagrams/slides/hidden_state/slide_2.png";
+    $image
+      .next("p")
+      .text("Typo is fixed, and cell is run again resulting in an error");
+    $nextButton.css("visibility", "hidden");
+  });
+
+  $resetButton.on("click", function(e) {
+    e.preventDefault();
+
+    $nextButton.css("visibility", "visible");
+    image.src = "images/diagrams/slides/hidden_state/slide_1.png";
+    $image.next("p").text("Cell is executed with a typo");
+  });
+});
+
+$(function() {
+  const $nextButton = $("#nextButton2");
+  const $resetButton = $("#resetButton2");
+
+  const image = document.getElementById("mutate-clone");
+  const $image = $("#mutate-clone");
+
+  $nextButton.on("click", function(e) {
+    e.preventDefault();
+
+    image.src = "images/diagrams/system_architecture/not_mutating_clone.png";
+    $image
+      .next("p")
+      .text(
+        "Using a flag on a cloned notebook, we can create a new ID on a subsequent save"
+      );
+    $nextButton.css("visibility", "hidden");
   });
 
   $resetButton.on("click", function(e) {
     e.preventDefault();
     $nextButton.prop("disabled", false);
-    image.src = "images/diagrams/slides/hidden_state/slide_1.png";
-    $image.next("p").text("Cell is executed with a typo.");
-  });
-});
-
-$(function() {
-  const $nextButton2 = $("#nextSystemBtn");
-  const $resetButton2 = $("#resetSystemBtn");
-  const image = document.getElementById("log-collection");
-
-  $nextButton2.on("click", function(e) {
-    e.preventDefault();
-
-    if (image.src.match("log_collection.png")) {
-      image.src = "images/diagrams/log_collection/log_collection_rsyslog.png";
-    } else if (image.src.match("log_collection_rsyslog")) {
-      image.src = "images/diagrams/log_collection/log_collection_syslogng.png";
-    } else if (image.src.match("log_collection_syslogng")) {
-      $nextButton.prop("disabled", true);
-    }
-  });
-
-  $resetButton2.on("click", function(e) {
-    e.preventDefault();
-    $nextButton2.prop("disabled", false);
-    image.src = "images/diagrams/log_collection/log_collection.png";
+    $nextButton.css("visibility", "visible");
+    image.src = "images/diagrams/system_architecture/mutating_clone.png";
+    $image
+      .next("p")
+      .text(
+        "Without a way to flag a cloned notebook, subsequent saves will mutate the original"
+      );
   });
 });
 
